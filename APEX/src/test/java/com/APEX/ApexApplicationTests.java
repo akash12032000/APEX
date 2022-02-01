0e28ae0d29b284eaaf49c89ed97b23c436ed58eb
Acutec <Acutec@192.168.1.26>
2022-02-01 10:43:45 +0530

    First Commit

---
+++ APEX/src/test/java/com/APEX/ApexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
